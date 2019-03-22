65a94085c8537ba7bd796ac902ac450a2417867c
Amruta <amrutapatil1720@gmail.com>
2019-03-21 19:06:23 -0700

    Updates

---
+++ precache-manifest.3a2f3ff9c16b84c596096a72b54b1099.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1a5b208a45cfbb23679d1c2dc690605",
+    "url": "/React_Portfolio/static/media/profile.b1a5b208.jpg"
+  },
+  {
+    "revision": "b6aa8232b4d54c3ee476",
+    "url": "/React_Portfolio/static/js/runtime~main.b6aa8232.js"
+  },
+  {
+    "revision": "be2145188ec61b700906",
+    "url": "/React_Portfolio/static/js/main.be214518.chunk.js"
+  },
+  {
+    "revision": "65824d836b3b354cb449",
+    "url": "/React_Portfolio/static/js/2.65824d83.chunk.js"
+  },
+  {
+    "revision": "be2145188ec61b700906",
+    "url": "/React_Portfolio/static/css/main.2b659361.chunk.css"
+  },
+  {
+    "revision": "65824d836b3b354cb449",
+    "url": "/React_Portfolio/static/css/2.8df55d8a.chunk.css"
+  },
+  {
+    "revision": "9828195d41d7023096d3f6637cc25ce8",
+    "url": "/React_Portfolio/index.html"
+  }
+];
